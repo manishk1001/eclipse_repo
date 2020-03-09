0c84e8d36d26300cf1ea5f0e43b6bea0ec5c80b8
manish <k.manish9973@gmail.com>
2020-03-09 10:23:57 +0530

    Edited by Developer 2

--- MobileProject/src/com/capgemini/ui/Main.java
+++ MobileProject/src/com/capgemini/ui/Main.java
@@ -3,7 +3,7 @@ package com.capgemini.ui;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("added by developer 2");
 
 	}
 
